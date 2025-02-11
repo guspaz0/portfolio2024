63a7ecce4ad2d22f9fa3ac8c40ff5b8d855fa022
guspaz0 <gusti.paz@gmail.com>
2025-02-11 19:27:50 -0300

    fix: router

--- src/app.js
+++ src/app.js
@@ -1,67 +1,11 @@
-import navmain from './components/nav.js'
 import pie from './components/pie.js'
 import router from './router.js'
-import home from './components/home.js'
 
 const{ createApp } = Vue
 
 const app = createApp({
-    template: `
-        <navmain 
-            :darkmode="darkmode" @setdarkmode="setdarkmode"
-            :profile="profile" @setProfile="setProfile"
-        ></navmain>
-        <home :profile="profile" @setProfile="setProfile"></home>
-        <pie></pie>
-    `,
     components: {
-        'navmain': navmain,
-        'pie': pie,
-        'home': home
-    },
-    data() {
-        return {
-            darkmode: false,
-            profile: null
-        }
-    },
-    methods: {
-        setdarkmode(){
-            this.darkmode = !this.darkmode
-        },
-        colorSchema(mode) {
-            return window.matchMedia(`(prefers-color-scheme: ${mode})`).matches
-        },
-        setProfile(id){
-            sessionStorage.setItem('profileId', id)
-            this.profile = +id
-        }
-    },
-    watch: {
-        darkmode(val){
-            const appTheme = document.documentElement
-            if (val) {
-                appTheme.classList.add('light')
-                appTheme.classList.remove('dark') 
-            } else {
-                appTheme.classList.remove('light')
-                appTheme.classList.add('dark') 
-            }
-        }
-    },
-    mounted(){
-        const appTheme = document.documentElement
-        if (this.colorSchema('dark')) {
-            this.darkmode = false
-            appTheme.classList.remove('light')
-            appTheme.classList.add('dark') 
-        }
-        else {
-            this.darkmode = true
-            appTheme.classList.remove('light')
-            appTheme.classList.add('dark') 
-        }
-        this.profile = sessionStorage.getItem('profile') || 1;
+        'pie': pie
     },
     created(){
         this.$router.push({
